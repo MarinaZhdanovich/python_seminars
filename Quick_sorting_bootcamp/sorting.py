8a2543ebc13e6e0935f96f02207ab6c64f1c95fa
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2023-08-07 14:02:54 +0300

    Quick Sorting

---
+++ Quick_sorting_bootcamp/sorting.py
@@ -0,0 +1,15 @@
+def quick_sorting(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+
+        left = [x for x in arr[1:] if x < pivot]
+        right = [x for x in arr[1:] if x >= pivot]
+
+        return quick_sorting(left) + [pivot] + quick_sorting(right)
+
+
+array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+sorted_array = quick_sorting(array)
+print(sorted_array)
